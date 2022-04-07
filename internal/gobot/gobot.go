127f4ebd0b23c30da155f3bf10ad965e18676247
Sergey <he305@mail.ru>
2022-04-07 20:04:43 +0300

    Tidying up and adding prestart funcs

--- internal/gobot/gobot.go
+++ internal/gobot/gobot.go
@@ -3,6 +3,7 @@ package gobot
 import (
 	"fmt"
 	"gobot/internal/anime/animefeeder"
+	"gobot/internal/anime/releasestorage"
 	"gobot/internal/anime/releasestorage/filereleasestorage"
 	"gobot/pkg/animeservice"
 	"gobot/pkg/animeservice/malv2service"
@@ -10,6 +11,8 @@ import (
 	"gobot/pkg/animeurlfinder/subspleaserss"
 	"gobot/pkg/logging"
 	"os"
+	"path/filepath"
+	"strings"
 	"time"
 
 	tgbot "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -38,6 +41,54 @@ func initLogger() {
 	logging.InitLoggerConfig(rawJson)
 }
 
+func getInfoForPrinting(animeFeeder animefeeder.AnimeFeeder, storage releasestorage.ReleaseStorage) (st string) {
+
+	missingInCached, missingInNew := animeFeeder.UpdateList()
+	if missingInCached != nil {
+		st += "New entries in list\n"
+		for _, v := range missingInCached {
+			st += v.VerboseOutput()
+			st += "\n"
+		}
+	}
+
+	if missingInNew != nil {
+		st += "Entries were deleted\n"
+		for _, v := range missingInNew {
+			v.ListStatus = animeservice.NotInList
+			st += v.VerboseOutput()
+			st += "\n"
+		}
+	}
+
+	latestReleases := animeFeeder.FindLatestReleases()
+	newReleases := storage.UpdateStorage(latestReleases)
+
+	for _, v := range newReleases {
+		if v.AnimeUrl.Url != "" {
+			st += fmt.Sprintf("New release for %s\nanime url: %s\n", v.Anime.Title, v.AnimeUrl.Url)
+		}
+		if v.SubsUrl.Url != "" {
+			st += fmt.Sprintf("New subs for %s\nurl: %s\n", v.Anime.Title, v.SubsUrl.Url)
+		}
+	}
+
+	return st
+}
+
+func createPath(path string) error {
+	_, err := os.Stat(path)
+	if !os.IsNotExist(err) {
+		return nil
+	}
+
+	if err := os.MkdirAll(filepath.Dir(path), 0770); err != nil {
+		return err
+	}
+	_, err = os.Create(path)
+	return err
+}
+
 func Run() {
 	initLogger()
 	logger = logging.GetLogger()
@@ -49,11 +100,21 @@ func Run() {
 	malv2password := viper.GetString("malv2password")
 	telegramChatId := viper.GetInt64("telegramChatId")
 
+	kitsunekkoCachePath := viper.GetString("kitsunekkoCachePath")
+	releaseStoragePath := viper.GetString("releaseStoragePath")
+
+	if err := createPath(kitsunekkoCachePath); err != nil {
+		logger.Panicf("Couldn't create path %s, fatal error", kitsunekkoCachePath)
+	}
+	if err := createPath(releaseStoragePath); err != nil {
+		logger.Panicf("Couldn't create path %s, fatal error", releaseStoragePath)
+	}
+
 	malserv := malv2service.NewMalv2Service(malv2username, malv2password)
-	kitsunekkoSubService := kitsunekko.NewKitsunekkoScrapper()
+	kitsunekkoSubService := kitsunekko.NewKitsunekkoScrapper(kitsunekkoCachePath, 5*time.Minute)
 	subspleaserss := subspleaserss.NewSubsPleaseRss()
 
-	storage := filereleasestorage.NewFileReleaseStorage("./storage/test.txt")
+	storage := filereleasestorage.NewFileReleaseStorage(releaseStoragePath)
 
 	animeFeeder := animefeeder.NewAnimeFeeder(malserv, kitsunekkoSubService, subspleaserss)
 
@@ -74,48 +135,31 @@ func Run() {
 
 	go func() {
 		for {
-			missingInCached, missingInNew := animeFeeder.UpdateList()
-			if missingInCached != nil {
-				var st string
-				st += "New entries in list\n"
-				for _, v := range missingInCached {
-					st += v.VerboseOutput()
-					st += "\n"
-				}
+			st := getInfoForPrinting(animeFeeder, storage)
 
+			if st != "" {
 				msg := tgbot.NewMessage(telegramChatId, st)
 				bot.Send(msg)
 			}
 
-			if missingInNew != nil {
-				var st string
-				st += "Entries were deleted\n"
-				for _, v := range missingInNew {
-					v.ListStatus = animeservice.NotInList
-					st += v.VerboseOutput()
-					st += "\n"
-				}
-
-				msg := tgbot.NewMessage(telegramChatId, st)
-				bot.Send(msg)
-			}
-
-			latestReleases := animeFeeder.FindLatestReleases()
-			newReleases := storage.UpdateStorage(latestReleases)
-
-			for _, v := range newReleases {
-				fmt.Println(v)
-			}
-			time.Sleep(15 * time.Second)
+			time.Sleep(3 * time.Minute)
 		}
 	}()
 
 	updates := bot.GetUpdatesChan(u)
 	for update := range updates {
 		if update.Message != nil {
-			msg := tgbot.NewMessage(update.Message.Chat.ID, update.Message.Text)
-			msg.ReplyToMessageID = update.Message.MessageID
-			bot.Send(msg)
+
+			// Just for debug
+			charArray := []byte(update.Message.Text)
+			if charArray[0] == '/' {
+				splittedMessage := strings.Split(update.Message.Text, " ")
+				if len(splittedMessage) > 1 && splittedMessage[0] == "/anime" {
+					entry := malserv.GetAnimeByTitle(strings.Join(splittedMessage[1:], " "))
+					msg := tgbot.NewMessage(update.Message.Chat.ID, entry.VerboseOutput())
+					bot.Send(msg)
+				}
+			}
 		}
 	}
 }
