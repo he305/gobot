dbb32ad846fe68b04ba112f1f8d9c8c484a34a02
Sergey <he305@mail.ru>
2022-04-07 18:34:02 +0300

    Added cached storage

--- pkg/animesubs/kitsunekko/kitsunekko.go
+++ pkg/animesubs/kitsunekko/kitsunekko.go
@@ -4,6 +4,9 @@ import (
 	"gobot/pkg/animesubs"
 	"gobot/pkg/logging"
 	"gobot/pkg/stringutils"
+	"io"
+	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -12,23 +15,32 @@ import (
 )
 
 type kitsunekkoScrapper struct {
-	logger *zap.SugaredLogger
+	logger          *zap.SugaredLogger
+	collector       *colly.Collector
+	lastTimeUpdated time.Time
+	updateTimer     time.Duration
+	cachedFilePath  string
 }
 
 var _ animesubs.AnimeSubsService = (*kitsunekkoScrapper)(nil)
 var KitsunekkoTimeLayout = "Jan 02 2006 3:04:05 PM"
+var kitsunekkoBaseUrl = "https://kitsunekko.net"
 var kitsunekkoJapBaseUrl = "https://kitsunekko.net/dirlist.php?dir=subtitles%2Fjapanese%2F"
 
-func NewKitsunekkoScrapper() animesubs.AnimeSubsService {
-	return &kitsunekkoScrapper{logger: logging.GetLogger()}
+func NewKitsunekkoScrapper(cachedFilePath string, updateTimer time.Duration) animesubs.AnimeSubsService {
+	collector := colly.NewCollector()
+	collector.AllowURLRevisit = true
+	t := &http.Transport{}
+	t.RegisterProtocol("file", http.NewFileTransport(http.Dir(".")))
+	collector.WithTransport(t)
+
+	return &kitsunekkoScrapper{logger: logging.GetLogger(), collector: collector, updateTimer: updateTimer, cachedFilePath: cachedFilePath, lastTimeUpdated: time.Unix(0, 0)}
 }
 
 func (ws *kitsunekkoScrapper) getRequiredAnimeUrl(titles []string) string {
 	var founded []animesubs.SubsInfo
 
-	collector := colly.NewCollector()
-
-	collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
+	ws.collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
 
 		text := strings.ToLower(e.Text)
 
@@ -46,15 +58,15 @@ func (ws *kitsunekkoScrapper) getRequiredAnimeUrl(titles []string) string {
 
 				founded = append(founded, animesubs.SubsInfo{
 					Title:       text,
-					Url:         e.Request.AbsoluteURL(e.Attr("href")),
+					Url:         e.Attr("href"),
 					TimeUpdated: parsedTime,
 				})
 			}
 		}
 	})
 
-	if err := collector.Visit(kitsunekkoJapBaseUrl); err != nil {
-		ws.logger.Errorf("Error acquiring kitsunekko sub, url: %s, error: %s", kitsunekkoJapBaseUrl, err.Error())
+	if err := ws.collector.Visit("file://" + ws.cachedFilePath); err != nil {
+		ws.logger.Errorf("Error acquiring kitsunekko sub, url: %s, error: %s", ws.cachedFilePath, err.Error())
 		return ""
 	}
 
@@ -77,16 +89,20 @@ func (ws *kitsunekkoScrapper) getRequiredAnimeUrl(titles []string) string {
 }
 
 func (ws *kitsunekkoScrapper) GetUrlLatestSubForAnime(titlesWithSynonyms []string) animesubs.SubsInfo {
+	err := ws.updateCache()
+	if err != nil {
+		ws.logger.Errorf("Error updating kitsunekko base site, error: %v", err)
+		return animesubs.SubsInfo{}
+	}
+
 	requiredUrl := ws.getRequiredAnimeUrl(titlesWithSynonyms)
 	if requiredUrl == "" {
 		return animesubs.SubsInfo{}
 	}
 
-	collector := colly.NewCollector()
-
 	var en animesubs.SubsInfo
 	latestTime := time.Unix(0, 0)
-	collector.OnHTML("td.tdright", func(e *colly.HTMLElement) {
+	ws.collector.OnHTML("td.tdright", func(e *colly.HTMLElement) {
 		timeSt := e.Attr("title")
 
 		parsedTime, err := time.Parse(KitsunekkoTimeLayout, timeSt)
@@ -112,11 +128,36 @@ func (ws *kitsunekkoScrapper) GetUrlLatestSubForAnime(titlesWithSynonyms []strin
 	})
 
 	// Let's sleep for some time before requesting second url
-	time.Sleep(100 * time.Millisecond)
-	if err := collector.Visit(requiredUrl); err != nil {
-		ws.logger.Errorf("Error acquiring kitsunekko sub, url: %s, error: %s", requiredUrl, err.Error())
+	time.Sleep(50 * time.Millisecond)
+	if err := ws.collector.Visit(kitsunekkoBaseUrl + requiredUrl); err != nil {
+		ws.logger.Errorf("Error acquiring kitsunekko sub, url: %s, error: %s", kitsunekkoBaseUrl+requiredUrl, err.Error())
 		return animesubs.SubsInfo{}
 	}
 
 	return en
+}
+
+func (ws *kitsunekkoScrapper) updateCache() error {
+	if time.Now().Sub(ws.lastTimeUpdated) >= ws.updateTimer {
+		resp, err := http.Get(kitsunekkoJapBaseUrl)
+		if err != nil {
+			return err
+		}
+
+		defer resp.Body.Close()
+		f, err := os.OpenFile(ws.cachedFilePath, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		_, err = io.Copy(f, resp.Body)
+		if err != nil {
+			return err
+		}
+		ws.lastTimeUpdated = time.Now()
+
+		ws.logger.Infow("Kitsunekko index html was cached")
+	}
+
+	return nil
 }
