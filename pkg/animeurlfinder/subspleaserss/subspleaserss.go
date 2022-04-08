348d792b62739c7611bfecbb871fcdd0831cdbcb
Sergey <he305@mail.ru>
2022-04-08 15:55:57 +0300

    Refactoring and test covering

--- pkg/animeurlfinder/subspleaserss/subspleaserss.go
+++ pkg/animeurlfinder/subspleaserss/subspleaserss.go
@@ -1,9 +1,7 @@
 package subspleaserss
 
 import (
-	"fmt"
 	"gobot/pkg/animeurlfinder"
-	"gobot/pkg/logging"
 	"gobot/pkg/stringutils"
 	"regexp"
 	"strings"
@@ -13,85 +11,156 @@ import (
 	"go.uber.org/zap"
 )
 
-var rss1080Url = "https://subsplease.org/rss/?t&r=1080"
+const Rss1080Url = "https://subsplease.org/rss/?t&r=1080"
+
 var rssSubsPleaseTimeLayout = "Mon, 02 Jan 2006 15:04:05 -0700"
 var subspleaseRssPrefix = "[SubsPlease]"
 var levenshteinPercentMin = 70
+
+type subsPleaseRssEntry struct {
+	Text        string
+	RealText    string
+	TimeUpdated time.Time
+	Url         string
+}
 
 type subspleaserss struct {
 	parser      *gofeed.Parser
 	cachedFeed  *gofeed.Feed
 	logger      *zap.SugaredLogger
 	lastUpdated time.Time
+	updateTimer time.Duration
+	feedUrl     string
 }
 
 var _ animeurlfinder.AnimeUrlFinder = (*subspleaserss)(nil)
 
-func NewSubsPleaseRss() animeurlfinder.AnimeUrlFinder {
-	return &subspleaserss{parser: gofeed.NewParser(), logger: logging.GetLogger(), cachedFeed: nil}
+func NewSubsPleaseRss(feedUrl string, updateTimer time.Duration, logger *zap.SugaredLogger) animeurlfinder.AnimeUrlFinder {
+	return &subspleaserss{
+		parser:      gofeed.NewParser(),
+		logger:      logger,
+		cachedFeed:  nil,
+		updateTimer: updateTimer,
+		lastUpdated: time.Unix(0, 0),
+		feedUrl:     feedUrl,
+	}
 }
 
-func (s *subspleaserss) updateFeed() {
-	if s.cachedFeed == nil || time.Now().Sub(s.lastUpdated).Minutes() > 3 {
-		feed, err := s.parser.ParseURL(rss1080Url)
+func parseFeed(parser *gofeed.Parser, url string) (*gofeed.Feed, error) {
+	return parser.ParseURL(url)
+}
 
+func (s *subspleaserss) updateFeed() error {
+	if time.Now().Sub(s.lastUpdated) < s.updateTimer {
+		return nil
+	}
+
+	feed, err := parseFeed(s.parser, s.feedUrl)
+
+	if err != nil {
+		return err
+	}
+
+	s.cachedFeed = feed
+	s.lastUpdated = time.Now()
+
+	return nil
+}
+
+func parseSubsPleaseTime(timeString string) (time.Time, error) {
+	return time.Parse(rssSubsPleaseTimeLayout, timeString)
+}
+
+func parseSubsPleaseRssEntry(item *gofeed.Item) (subsPleaseRssEntry, error) {
+	parsedTime, err := parseSubsPleaseTime(item.Published)
+	if err != nil {
+		return subsPleaseRssEntry{}, err
+	}
+
+	return subsPleaseRssEntry{
+		Text:        item.Title,
+		TimeUpdated: parsedTime,
+		Url:         item.Link,
+		RealText:    item.Title,
+	}, nil
+}
+
+func (s *subspleaserss) getAllRssEntries() []subsPleaseRssEntry {
+	var rawRss []subsPleaseRssEntry
+	for _, item := range s.cachedFeed.Items {
+		parsedEntry, err := parseSubsPleaseRssEntry(item)
 		if err != nil {
-			s.logger.Errorf("Could parse subs please rss url, url : %s, error: %s", rss1080Url, err.Error())
+			s.logger.Errorf("Error parsing entry %v, error: %v", item, err)
+			continue
 		}
-
-		s.cachedFeed = feed
-		s.lastUpdated = time.Now()
+		rawRss = append(rawRss, parsedEntry)
 	}
+	return rawRss
 }
 
-func (s *subspleaserss) GetLatestUrlForTitle(titlesWithSynonyms []string) animeurlfinder.AnimeUrlInfo {
-	s.updateFeed()
+func (s *subspleaserss) getNormalizedRssEntries() []subsPleaseRssEntry {
+	allEntries := s.getAllRssEntries()
 
-	for i := range titlesWithSynonyms {
-		titlesWithSynonyms[i] = strings.TrimSpace(titlesWithSynonyms[i])
-		titlesWithSynonyms[i] = strings.ToLower(titlesWithSynonyms[i])
+	for i := range allEntries {
+		allEntries[i].Text = normalizeRssTitle(allEntries[i].Text)
 	}
+	return allEntries
+}
 
-	var rawRssTitles []string
-	for _, rawTitle := range s.cachedFeed.Items {
-		rawRssTitles = append(rawRssTitles, rawTitle.Title)
-	}
-
-	normalizedRssTitles := normalizeRssTitles(rawRssTitles)
-
-	found := false
-	var idx int
-	for i, normalizedRssTitle := range normalizedRssTitles {
-		for _, title := range titlesWithSynonyms {
-			if isRssMatchingTitle(normalizedRssTitle, title) {
-				idx = i
-				found = true
-				break
+func filterEntriesByTitles(entries []subsPleaseRssEntry, titles ...string) []subsPleaseRssEntry {
+	var filtered []subsPleaseRssEntry
+	for _, entry := range entries {
+		for _, title := range titles {
+			title := stringutils.LowerAndTrimText(title)
+			if isRssMatchingTitle(entry.Text, title) {
+				filtered = append(filtered, entry)
 			}
 		}
+	}
+	return filtered
+}
 
-		if found {
-			break
+func findLatestPageEntry(entries []subsPleaseRssEntry) subsPleaseRssEntry {
+	if len(entries) == 0 {
+		return subsPleaseRssEntry{}
+	}
+
+	actualentry := entries[0]
+	latestTime := time.Unix(0, 0)
+
+	for _, entry := range entries {
+		if entry.TimeUpdated.After(latestTime) {
+			latestTime = entry.TimeUpdated
+			actualentry = entry
 		}
 	}
 
-	if found {
-		parsedTime, err := time.Parse(rssSubsPleaseTimeLayout, s.cachedFeed.Items[idx].Published)
-		if err != nil {
-			panic("Error parsing rss subsplease time using default time format, critical error: " + err.Error())
-		}
+	return actualentry
+}
 
-		return animeurlfinder.AnimeUrlInfo{
-			Title:       s.cachedFeed.Items[idx].Title,
-			TimeUpdated: parsedTime,
-			Url:         s.cachedFeed.Items[idx].Link,
-		}
+func (s *subspleaserss) GetLatestUrlForTitle(titlesWithSynonyms ...string) animeurlfinder.AnimeUrlInfo {
+	if err := s.updateFeed(); err != nil {
+		s.logger.Errorf("Could parse subs please rss url, url : %s, error: %s", s.feedUrl, err.Error())
+		return animeurlfinder.AnimeUrlInfo{}
 	}
 
-	return animeurlfinder.AnimeUrlInfo{}
+	normalizedRssTitles := s.getNormalizedRssEntries()
+
+	filteredEntries := filterEntriesByTitles(normalizedRssTitles, titlesWithSynonyms...)
+
+	actualentry := findLatestPageEntry(filteredEntries)
+
+	return animeurlfinder.AnimeUrlInfo{
+		Title:       actualentry.RealText,
+		TimeUpdated: actualentry.TimeUpdated,
+		Url:         actualentry.Url,
+	}
+
 }
 
 func isRssMatchingTitle(rss string, title string) bool {
+	// Normalize title
+	title = stringutils.LowerAndTrimText(title)
 	// First - simple matching
 	isMatching := stringutils.AreSecondContainsFirst(title, rss)
 	if isMatching {
@@ -103,26 +172,15 @@ func isRssMatchingTitle(rss string, title string) bool {
 	return percent >= levenshteinPercentMin
 }
 
-func normalizeRssTitles(titles []string) []string {
-	var normalizeRssTitles []string
+func normalizeRssTitle(title string) string {
+	re := regexp.MustCompile(`\[\w+\]`)
 
-	re, err := regexp.Compile(`\[\w+\]`)
+	title = re.ReplaceAllString(title, "")
+	title = strings.Replace(title, "(1080p)", "", -1)
+	title = strings.Replace(title, ".mkv", "", -1)
+	title = strings.ToLower(title)
+	title = strings.Join(strings.Fields(title), " ")
+	title = strings.TrimSpace(title)
 
-	// Bad, but will panic only if regexp module is upgraded
-	if err != nil {
-		panic(fmt.Sprintf("Could not compile regex in subsplease module, fatal error: %s", err.Error()))
-	}
-
-	for _, title := range titles {
-		title = re.ReplaceAllString(title, "")
-		title = strings.Replace(title, "(1080p)", "", -1)
-		title = strings.Replace(title, ".mkv", "", -1)
-		title = strings.ToLower(title)
-		title = strings.Join(strings.Fields(title), " ")
-		title = strings.TrimSpace(title)
-
-		normalizeRssTitles = append(normalizeRssTitles, title)
-	}
-
-	return normalizeRssTitles
+	return title
 }
--- pkg/animeurlfinder/subspleaserss/subspleaserss_test.go
+++ pkg/animeurlfinder/subspleaserss/subspleaserss_test.go
@@ -1,22 +1,48 @@
 package subspleaserss
 
 import (
+	"gobot/pkg/animeurlfinder"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 	"time"
+
+	"github.com/mmcdole/gofeed"
+	"go.uber.org/zap"
 )
 
-// func TestCorrectOuput(t *testing.T) {
-// 	rss := NewSubsPleaseRss()
+func newTestServer() *httptest.Server {
+	mux := http.NewServeMux()
 
-// 	expected := "https://nyaa.si/view/1510211/torrent"
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:subsplease="https://subsplease.org/rss"><channel><title>SubsPlease RSS</title><description>RSS feed for SubsPlease releases (1080p)</description><link>https://subsplease.org</link><atom:link href="http://subsplease.org/rss" rel="self" type="application/rss+xml"/><item><title>[SubsPlease] Hanyou no Yashahime (25-48) (1080p) [Batch]</title><link>https://nyaa.si/view/1511823/torrent</link><guid isPermaLink="false">YC7Q3L2TRKWLIIWKZ57VI3ZKE73IHMS5</guid><pubDate>Fri, 08 Apr 2022 08:47:24 +0000</pubDate><category>Hanyou no Yashahime - 1080</category><subsplease:size>32.95 GiB</subsplease:size></item><item><title>[SubsPlease] Shuumatsu no Harem (01-11) (1080p) [Batch]</title><link>https://nyaa.si/view/1511812/torrent</link><guid isPermaLink="false">VFJUCN7IVULFF4YMRZCKHCLABCM4DANA</guid><pubDate>Fri, 08 Apr 2022 07:04:57 +0000</pubDate><category>Shuumatsu no Harem - 1080</category><subsplease:size>14.79 GiB</subsplease:size></item><item><title>[SubsPlease] Girls&apos; Frontline (01-12) (1080p) [Batch]</title><link>https://nyaa.si/view/1511759/torrent</link><guid isPermaLink="false">ZSUMK22DN7YBEFG6UE4M7SG3OOJFJMZL</guid><pubDate>Fri, 08 Apr 2022 01:20:40 +0000</pubDate><category>Girls' Frontline - 1080</category><subsplease:size>13.08 GiB</subsplease:size></item></channel></rss>`))
+	})
 
-// 	actual := rss.GetLatestUrlForTitle("Shingeki no Kyojin")
-// 	fmt.Println(actual)
+	mux.HandleFunc("/brokentime", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:subsplease="https://subsplease.org/rss"><channel><title>SubsPlease RSS</title><description>RSS feed for SubsPlease releases (1080p)</description><link>https://subsplease.org</link><atom:link href="http://subsplease.org/rss" rel="self" type="application/rss+xml"/><item><title>[SubsPlease] Hanyou no Yashahime (25-48) (1080p) [Batch]</title><link>https://nyaa.si/view/1511823/torrent</link><guid isPermaLink="false">YC7Q3L2TRKWLIIWKZ57VI3ZKE73IHMS5</guid><pubDate>Fri, 08 Apr 2022 08:47:24</pubDate><category>Hanyou no Yashahime - 1080</category><subsplease:size>32.95 GiB</subsplease:size></item></channel></rss>`))
+	})
 
-// 	if actual != expected {
-// 		t.Errorf("expected: %v, got: %v", expected, actual)
-// 	}
-// }
+	mux.HandleFunc("/latest", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+		<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:subsplease="https://subsplease.org/rss"><channel><title>SubsPlease RSS</title><description>RSS feed for SubsPlease releases (1080p)</description><link>https://subsplease.org</link><atom:link href="http://subsplease.org/rss" rel="self" type="application/rss+xml"/>
+		<item><title>Hanyou no Yashahime 1</title><link>https://1</link><guid isPermaLink="false">YC7Q3L2TRKWLIIWKZ57VI3ZKE73IHMS5</guid><pubDate>Fri, 05 Apr 2022 08:47:24 +0000</pubDate><category>Hanyou no Yashahime - 1080</category><subsplease:size>32.95 GiB</subsplease:size></item>
+		<item><title>Hanyou no Yashahime 3</title><link>https://3</link><guid isPermaLink="false">YC7Q3L2TRKWLIIWKZ57VI3ZKE73IHMS5</guid><pubDate>Fri, 07 Apr 2022 08:47:24 +0000</pubDate><category>Hanyou no Yashahime - 1080</category><subsplease:size>32.95 GiB</subsplease:size></item>
+		<item><title>Hanyou no Yashahime 2</title><link>https://2</link><guid isPermaLink="false">YC7Q3L2TRKWLIIWKZ57VI3ZKE73IHMS5</guid><pubDate>Fri, 06 Apr 2022 08:47:24 +0000</pubDate><category>Hanyou no Yashahime - 1080</category><subsplease:size>32.95 GiB</subsplease:size></item></channel></rss>`))
+	})
+
+	return httptest.NewServer(mux)
+}
+
+func newTestPrepareScrapper() (*subspleaserss, *httptest.Server) {
+	server := newTestServer()
+	return &subspleaserss{
+		parser:      gofeed.NewParser(),
+		logger:      zap.L().Sugar(),
+		cachedFeed:  nil,
+		updateTimer: 0,
+		lastUpdated: time.Unix(0, 0),
+		feedUrl:     server.URL}, server
+}
 
 func TestNormalizeRssTitles(t *testing.T) {
 	data := []string{
@@ -30,7 +56,10 @@ func TestNormalizeRssTitles(t *testing.T) {
 		"baraou no souretsu - 12.5",
 	}
 
-	actual := normalizeRssTitles(data)
+	var actual []string
+	for _, d := range data {
+		actual = append(actual, normalizeRssTitle(d))
+	}
 
 	for i := 0; i < len(actual); i++ {
 		if actual[i] != expected[i] {
@@ -41,10 +70,9 @@ func TestNormalizeRssTitles(t *testing.T) {
 
 func TestSubsPleaseTimeParse(t *testing.T) {
 	data := "Tue, 05 Apr 2022 04:40:55 +0000"
-	timeLayout := "Mon, 02 Jan 2006 15:04:05 -0700"
 
 	expected := time.Date(2022, time.April, 5, 4, 40, 55, 0, time.UTC)
-	actual, err := time.Parse(timeLayout, data)
+	actual, err := parseSubsPleaseTime(data)
 	if err != nil {
 		t.Errorf("expected %v, got %v", expected, err.Error())
 	}
@@ -52,4 +80,256 @@ func TestSubsPleaseTimeParse(t *testing.T) {
 	if !actual.Equal(expected) {
 		t.Errorf("expected %v, got %v", expected, actual)
 	}
+}
+
+func TestIsRssMathingTitlesTrueSimpleMatching(t *testing.T) {
+	dataRss := "Shingeki no kyoujin"
+	dataTitle := "sHiNgEkI"
+
+	expected := true
+	actual := isRssMatchingTitle(dataRss, dataTitle)
+
+	if expected != actual {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+}
+
+func TestIsRssMathingTitlesTrueLevenshtein(t *testing.T) {
+	dataRss := "shingeki+no+kyoujin"
+	dataTitle := "shingeki no kyoujin"
+
+	expected := true
+	actual := isRssMatchingTitle(dataRss, dataTitle)
+
+	if expected != actual {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+}
+
+func TestUpdateFeedTooSoon(t *testing.T) {
+	sp, _ := newTestPrepareScrapper()
+
+	sp.lastUpdated = time.Now().Add(3 * time.Hour)
+
+	err := sp.updateFeed()
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+}
+
+func TestUpdateFeedBrokenUrl(t *testing.T) {
+	sp, _ := newTestPrepareScrapper()
+
+	sp.feedUrl = "/"
+
+	err := sp.updateFeed()
+	if err == nil {
+		t.Errorf("expected error, got none")
+	}
+}
+
+func TestUpdateFeedOk(t *testing.T) {
+	sp, _ := newTestPrepareScrapper()
+
+	err := sp.updateFeed()
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+}
+
+func TestGetAllEntriesOk(t *testing.T) {
+	sp, _ := newTestPrepareScrapper()
+
+	err := sp.updateFeed()
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+
+	allEntries := sp.getAllRssEntries()
+	if len(allEntries) != 3 {
+		t.Errorf("expected 3 entries, got %v", len(allEntries))
+	}
+}
+
+func TestGetAllEntriesBrokenTime(t *testing.T) {
+	sp, _ := newTestPrepareScrapper()
+	sp.feedUrl = sp.feedUrl + "/brokentime"
+	err := sp.updateFeed()
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+
+	allEntries := sp.getAllRssEntries()
+	if len(allEntries) != 0 {
+		t.Errorf("expected 0 entries, got %v", len(allEntries))
+	}
+}
+
+func TestGetNormalizedEntries(t *testing.T) {
+	sp, _ := newTestPrepareScrapper()
+
+	err := sp.updateFeed()
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+
+	expected := []string{
+		"hanyou no yashahime (25-48)",
+		"shuumatsu no harem (01-11)",
+		"girls' frontline (01-12)",
+	}
+
+	allEntries := sp.getNormalizedRssEntries()
+	if len(allEntries) != 3 {
+		t.Errorf("expected 3 entries, got %v", len(allEntries))
+	}
+
+	for i := range allEntries {
+		if allEntries[i].Text != expected[i] {
+			t.Errorf("expected %v, got %v", allEntries[i], expected)
+		}
+	}
+}
+
+func TestGetFilteredEntries(t *testing.T) {
+	sp, _ := newTestPrepareScrapper()
+
+	err := sp.updateFeed()
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	//  Girls&apos; Frontline (01-12) (1080p) [Batch]</title><link>https://nyaa.si/view/1511759/torrent</link><guid isPermaLink="false">ZSUMK22DN7YBEFG6UE4M7SG3OOJFJMZL</guid><pubDate>Fri, 08 Apr 2022 01:20:40 +0000
+	expected := subsPleaseRssEntry{
+		Text:        "girls' frontline (01-12)",
+		TimeUpdated: time.Date(2022, time.April, 8, 1, 20, 40, 0, time.UTC), //08 Apr 2022 01:20:40 +0000
+		Url:         "https://nyaa.si/view/1511759/torrent",
+		RealText:    "[SubsPlease] Girls' Frontline (01-12) (1080p) [Batch]",
+	}
+	data := "girls' frontline"
+
+	allEntries := sp.getNormalizedRssEntries()
+
+	actual := filterEntriesByTitles(allEntries, data)
+
+	if len(actual) != 1 {
+		t.Errorf("expected 1 entry, got %v", len(allEntries))
+	}
+
+	actualEn := actual[0]
+
+	if expected.Text != actualEn.Text ||
+		expected.RealText != actualEn.RealText ||
+		!expected.TimeUpdated.Equal(actualEn.TimeUpdated) ||
+		expected.Url != actualEn.Url {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+}
+
+func TestFindLatest(t *testing.T) {
+	sp, _ := newTestPrepareScrapper()
+	sp.feedUrl = sp.feedUrl + "/latest"
+	err := sp.updateFeed()
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	//Hanyou no Yashahime 3</title><link>https://3</link><guid isPermaLink="false">YC7Q3L2TRKWLIIWKZ57VI3ZKE73IHMS5</guid><pubDate>Fri,
+	//07 Apr 2022 08:47:24
+	expected := subsPleaseRssEntry{
+		Text:        "hanyou no yashahime 3",
+		TimeUpdated: time.Date(2022, time.April, 7, 8, 47, 24, 0, time.UTC), //08 Apr 2022 01:20:40 +0000
+		Url:         "https://3",
+		RealText:    "Hanyou no Yashahime 3",
+	}
+	data := "hanyou no yashahime"
+
+	allEntries := sp.getNormalizedRssEntries()
+
+	filtered := filterEntriesByTitles(allEntries, data)
+
+	if len(filtered) != 3 {
+		t.Errorf("expected 3 entry, got %v", len(allEntries))
+	}
+
+	actualEn := findLatestPageEntry(filtered)
+
+	if expected.Text != actualEn.Text ||
+		expected.RealText != actualEn.RealText ||
+		!expected.TimeUpdated.Equal(actualEn.TimeUpdated) ||
+		expected.Url != actualEn.Url {
+		t.Errorf("expected %v, got %v", expected, actualEn)
+	}
+}
+
+func TestFindLatestEmpty(t *testing.T) {
+	sp, _ := newTestPrepareScrapper()
+	sp.feedUrl = sp.feedUrl + "/latest"
+	err := sp.updateFeed()
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	//Hanyou no Yashahime 3</title><link>https://3</link><guid isPermaLink="false">YC7Q3L2TRKWLIIWKZ57VI3ZKE73IHMS5</guid><pubDate>Fri,
+	//07 Apr 2022 08:47:24
+	data := "some irellevant data"
+
+	allEntries := sp.getNormalizedRssEntries()
+
+	filtered := filterEntriesByTitles(allEntries, data)
+
+	if len(filtered) != 0 {
+		t.Errorf("expected 0 entry, got %v", len(allEntries))
+	}
+
+	actual := findLatestPageEntry(filtered)
+	if actual.Text != "" ||
+		actual.RealText != "" ||
+		actual.Url != "" {
+		t.Errorf("expected %v, got %v", nil, actual)
+	}
+}
+
+func TestGetLatestReleaseValid(t *testing.T) {
+	sp, _ := newTestPrepareScrapper()
+	sp.feedUrl = sp.feedUrl + "/latest"
+	err := sp.updateFeed()
+	if err != nil {
+		t.Errorf("expected no error, got %v", err)
+	}
+	//Hanyou no Yashahime 3</title><link>https://3</link><guid isPermaLink="false">YC7Q3L2TRKWLIIWKZ57VI3ZKE73IHMS5</guid><pubDate>Fri,
+	//07 Apr 2022 08:47:24
+	expected := animeurlfinder.AnimeUrlInfo{
+		TimeUpdated: time.Date(2022, time.April, 7, 8, 47, 24, 0, time.UTC), //08 Apr 2022 01:20:40 +0000
+		Url:         "https://3",
+		Title:       "Hanyou no Yashahime 3",
+	}
+	data := "hanyou no yashahime"
+
+	actualEn := sp.GetLatestUrlForTitle(data)
+
+	if expected.Title != actualEn.Title ||
+		!expected.TimeUpdated.Equal(actualEn.TimeUpdated) ||
+		expected.Url != actualEn.Url {
+		t.Errorf("expected %v, got %v", expected, actualEn)
+	}
+}
+
+func TestGetLatestReleaseBrokenUrl(t *testing.T) {
+	sp, _ := newTestPrepareScrapper()
+	sp.feedUrl = "/"
+	//Hanyou no Yashahime 3</title><link>https://3</link><guid isPermaLink="false">YC7Q3L2TRKWLIIWKZ57VI3ZKE73IHMS5</guid><pubDate>Fri,
+	//07 Apr 2022 08:47:24
+
+	data := "hanyou no yashahime"
+
+	actualEn := sp.GetLatestUrlForTitle(data)
+
+	if "" != actualEn.Title ||
+		"" != actualEn.Url {
+		t.Errorf("expected %v, got %v", nil, actualEn)
+	}
+}
+
+func TestConstructor(t *testing.T) {
+	url := "/test"
+	duration := time.Duration(3 * time.Minute)
+	NewSubsPleaseRss(url, duration, zap.L().Sugar())
 }
