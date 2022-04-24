da49ef066cd2ae6a81c44da12f8f044d2c14a553
Sergey Chernyshov <he305@mail.ru>
2022-04-24 14:27:53 +0300

    Removing release storage

--- internal/anime/releasestorage/releasestorage.go
+++
@@ -1,7 +0,0 @@
-package releasestorage
-
-import "gobot/internal/anime/animefeeder"
-
-type ReleaseStorage interface {
-	UpdateStorage([]animefeeder.LatestReleases) (newEntries []animefeeder.LatestReleases)
-}
