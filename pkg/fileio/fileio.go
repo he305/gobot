df9640a262d7da12a906463474c8f18e11b73617
Sergey <he305@mail.ru>
2022-04-24 19:20:32 +0300

    More funcs for fileio

--- pkg/fileio/fileio.go
+++ pkg/fileio/fileio.go
@@ -2,15 +2,35 @@ package fileio
 
 import (
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
 
 type FileIO interface {
 	SaveResponseToFile(data *http.Response, filePath string) error
+	SaveToFile(data []byte, filePath string) error
+	AppendToFile(data []byte, filePath string) error
+	ReadFile(filePath string) ([]byte, error)
 }
 
 type fileIo struct{}
+
+// ReadFile implements FileIO
+func (*fileIo) ReadFile(filePath string) ([]byte, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return content, nil
+}
 
 var _ FileIO = (*fileIo)(nil)
 
@@ -18,16 +38,37 @@ func NewDefaultFileIO() FileIO {
 	return &fileIo{}
 }
 
-func (f *fileIo) SaveResponseToFile(data *http.Response, filePath string) error {
-	defer data.Body.Close()
+func writeToFile(file *os.File, data []byte) error {
+	_, err := file.Write(data)
+	return err
+}
+
+func (f *fileIo) SaveToFile(data []byte, filePath string) error {
 	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-	_, err = io.Copy(file, data.Body)
+
+	return writeToFile(file, data)
+}
+
+func (f *fileIo) AppendToFile(data []byte, filePath string) error {
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_WRONLY, 0666)
 	if err != nil {
 		return err
 	}
-	return nil
+	defer file.Close()
+
+	return writeToFile(file, data)
+}
+
+func (f *fileIo) SaveResponseToFile(data *http.Response, filePath string) error {
+	defer data.Body.Close()
+	byteData, err := ioutil.ReadAll(data.Body)
+	if err != nil {
+		return err
+	}
+	err = f.SaveToFile(byteData, filePath)
+	return err
 }
